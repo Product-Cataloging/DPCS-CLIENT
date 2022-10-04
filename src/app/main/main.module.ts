a51c7e682d653a0327e0a9695bb07e623422b2ed
“Abel <abelgegz@gmail.com>
2022-10-04 10:24:10 +0300

    main module created. It includes layout and menu container components and header and footer ui components

---
+++ src/app/main/main.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MainRoutingModule } from './main-routing.module';
+import { HeaderComponent } from './components/ui/header/header.component';
+import { FooterComponent } from './components/ui/footer/footer.component';
+import { LayoutComponent } from './components/container/layout/layout.component';
+import { MenuComponent } from './components/container/menu/menu.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent,
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    MainRoutingModule
+  ]
+})
+export class MainModule { }
