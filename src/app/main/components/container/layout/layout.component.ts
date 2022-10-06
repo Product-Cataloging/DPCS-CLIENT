5ace952a88cc7e3cbe4f757d3138a410bb936bf0
“Abel <abelgegz@gmail.com>
2022-10-06 15:48:12 +0300

    Design changes to the Main navigation(included burger menu). Product list and cart list design changes.

--- src/app/main/components/container/layout/layout.component.ts
+++ src/app/main/components/container/layout/layout.component.ts
@@ -12,4 +12,8 @@ export class LayoutComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onMenuClick(){
+    console.log('Hello');
+  }
+ 
 }
--- src/app/main/components/ui/header/header.component.ts
+++ src/app/main/components/ui/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -8,10 +8,13 @@ import { Component, Input, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   @Input() orderCount: number = 0;
+  @Output() menuClick = new EventEmitter<any>;
   value:string = '';  
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  onMenuClick(){
+    this.menuClick.emit();
+  }
 }
