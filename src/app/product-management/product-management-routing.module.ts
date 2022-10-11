b322169454bd989df6dfc26d8c49f1f56ae17832
“Abel <abelgegz@gmail.com>
2022-10-11 15:18:22 +0300

    Quote components created. And quote is added to the route.

--- src/app/product-management/product-management-routing.module.ts
+++ src/app/product-management/product-management-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartListComponent } from './components/container/cart-list/cart-list.component';
 import { ProductDetailsComponent } from './components/container/product-details/product-details.component';
 import { ProductListComponent } from './components/container/product-list/product-list.component';
+import { QuoteComponent } from './components/container/quote/quote.component';
 const routes: Routes = [
   { path: '', component: ProductListComponent },
   { path:'product_items/:id',component:ProductDetailsComponent},
   { path:'cart', component:CartListComponent},
-  { path:'category/products/:id',component:ProductListComponent}
+  { path:'category/products/:id',component:ProductListComponent},
+  { path:'quote',component:QuoteComponent}
 
 ];
 
